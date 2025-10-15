c964d17c9df291b5f8646af1a47c93e8016dee09
Faraz Rabbani <farazrabbani575@gmail.com>
2025-10-15 22:31:15 +0500

    15/10/2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:fitform_ai/App/modules/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'app/modules/splash/splash_screen.dart';
 
 void main(){
   runApp(const GetMaterialApp(debugShowCheckedModeBanner: false,home: SplashScreen(),));
